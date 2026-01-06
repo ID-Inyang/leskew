562af9f1ba2ed5ff4649bd5b40676fb93b81ab50
BRID19 <74292639+BRID19@users.noreply.github.com>
2026-01-06 19:33:30 +0100

    commit before deployment on render

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useAuth } from "../context/AuthContext"
 import { assets } from '../assets/assets';
 import { Link } from "react-router-dom";
+import Typewriter from "typewriter-effect";
 
 const Hero = () => {
     const { user } = useAuth();
@@ -11,9 +12,19 @@ const Hero = () => {
         <div className="flex flex-col md:flex-row flex-wrap bg-gradient-to-r from-[#1F518C] to-[#26BBBF] text-white px-6 md:px-10 lg:px-20 w-full">
           {/* Left Side */}
           <div className='md:w-1/2 flex flex-col items-center justify-center gap-4 py-10 md:py-[10vw] md:mb-[-30px] text-3xl'>
-          <h1 className="text-5xl font-bold mb-4">
-            Welcome to <span className="font-stars ">Leskew</span>
-          </h1>
+          <div className="text-5xl font-stars font-bold mb-4 text-white">
+          <Typewriter
+  onInit={(typewriter) => {
+    typewriter
+      .typeString('<span class="font-stars text-5xl">Welcome to Leskew</span>')
+      .start(); // 👈 no delete, just stop after typing
+  }}
+/>
+
+        </div>
+
+
+
           <p className="text-xl  mb-8 max-w-2xl mx-auto">
             Book appointments and join real-time virtual queues
             for your favorite local businesses from anywhere.
@@ -57,7 +68,7 @@ const Hero = () => {
           </div>
           {/* -------- Right Side -------- */}
             <div className='md:w-1/2  flex justify-center relative'>
-                <img className='md:absolute right-0 bottom-5 w-[220px] md:w-[250px] md:h-[400px] rounded-lg' src={assets.phone_forward} alt="" />
+                <img className='md:absolute  right-0 bottom-5 w-[220px] md:w-[250px] md:h-[400px] rounded-lg' src={assets.phone_forward} alt="" />
             </div>
         
         </div></div>
